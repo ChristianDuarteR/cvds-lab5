88aaeaefe4274c7dc1357ed3a7b79545a800defd
unknown <johann.amaya@labinfo.is.escuelaing.edu.co>
2023-09-14 18:54:34 -0500

    Iniciacion de Api

--- src/main/java/com/example/servingwebcontent/UserController.java
+++ src/main/java/com/example/servingwebcontent/UserController.java
@@ -5,10 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-public class GreetingController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-    @GetMapping("/greeting")
+@Controller
+public class UserController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+
+    private String hello(){
+        return "Hello World";
+    }
+
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
